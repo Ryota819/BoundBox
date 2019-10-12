6058bbe0b5f16fc0f8ffa08979cccfb5172263bb
Ryota819 <ryou819@gmail.com>
2019-10-12 23:49:24 +0900

    miss

--- back-end/BoundBoxApi/BoundBox/settings.py
+++ back-end/BoundBoxApi/BoundBox/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'boundbox',
         'USER': 'boundboxadmin',
         'PASSWORD': 'power0f2',
-        'HOST': 'db_container',
+        'HOST': 'db-container',
         'PORT': '',
     }
 }
