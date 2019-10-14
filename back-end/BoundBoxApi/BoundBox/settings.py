8c861387dea6eb399111ac7f8148222376da3b9d
Ryota819 <ryou819@gmail.com>
2019-10-14 14:45:02 +0900

    修正DB

--- back-end/BoundBoxApi/BoundBox/settings.py
+++ back-end/BoundBoxApi/BoundBox/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'BoundBox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'boundbox',
-#         'USER': 'boundboxadmin',
-#         'PASSWORD': 'power0f2',
-#         'HOST': 'postgres',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'boundbox',
+        'USER': 'boundboxadmin',
+        'PASSWORD': 'power0f2',
+        'HOST': 'postgres',
+        'PORT': '',
+    }
+}
 
 # REST_FRAMEWORK = {
 #    'DEFAULT_PERMISSION_CLASSES': (
