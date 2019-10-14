0891e68bdcd4189d69f6b5909e360dfd8c2d3e37
Ryota819 <ryou819@gmail.com>
2019-10-14 14:33:38 +0900

    Allowed_hosts

--- back-end/BoundBoxApi/BoundBox/settings.py
+++ back-end/BoundBoxApi/BoundBox/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = '0q-s$+m!5jyxtct=(u8_76g2zf4xj#vm6e27s-x6j3j6m)7q4-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['35.224.46.100']
+ALLOWED_HOSTS = ['boundbox.tk']
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
